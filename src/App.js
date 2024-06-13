e34684ccf51ee3de30e6153e869a13942196f7e3
Wahyu <firmansyahw503@gmail.com>
2024-06-13 10:08:00 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './kucing.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          miawww miaww miaww miawww....
+        </p>
+        <a
+          className="App-link"
+          href="https://www.pngegg.com/id/png-vemev"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Loh Heh
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
